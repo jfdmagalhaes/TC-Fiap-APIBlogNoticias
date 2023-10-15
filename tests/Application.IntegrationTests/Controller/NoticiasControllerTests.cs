6b7a01f37ec2efc84629bc04b6a168932bbe6df2
Jessica <jessica.magalhaes@me.com.br>
2023-10-15 16:36:56 -0300

    Added initial integration tests

---
+++ tests/Application.IntegrationTests/Controller/NoticiasControllerTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using System.Net;
+using WebApi;
+
+namespace Application.IntegrationTests.Controller;
+
+[TestFixture]
+public class NoticiasControllerTests
+{
+    private HttpClient _client;
+    private TestServer _server;
+
+    [SetUp]
+    public void Setup()
+    {
+        _server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+        _client = _server.CreateClient();
+    }
+
+    [Test]
+    public async Task GetAllAsync_Returns_NotFound()
+    {
+        // Arrange
+        var requestUri = "/api/Noticias/GetAll";
+
+        // Act
+        var response = await _client.GetAsync(requestUri);
+
+        // Assert
+        Assert.AreEqual(HttpStatusCode.NotFound, response.StatusCode);
+    }
+}
