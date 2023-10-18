e5e01ce7ba31d9e0a52be8e20dcfc35994847e83
Jessica <jessica.magalhaes@me.com.br>
2023-10-17 21:01:23 -0300

    WIP Integration tests

--- tests/Application.IntegrationTests/Controller/NoticiasControllerTests.cs
+++ tests/Application.IntegrationTests/Controller/NoticiasControllerTests.cs
@@ -1,33 +1,42 @@
-﻿using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.TestHost;
-using System.Net;
-using WebApi;
+﻿using Domain.Entities;
+using Domain.Repositories;
+using FluentAssertions;
+using Moq;
+using WebApi.Controllers;
 
 namespace Application.IntegrationTests.Controller;
 
 [TestFixture]
 public class NoticiasControllerTests
 {
-    private HttpClient _client;
-    private TestServer _server;
+    private NoticiasController _controller;
+    private Mock<INoticiaRepository> _repositoryMock;
 
     [SetUp]
     public void Setup()
     {
-        _server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
-        _client = _server.CreateClient();
+        _repositoryMock = new Mock<INoticiaRepository>();
+        _controller = new NoticiasController(_repositoryMock.Object);
     }
 
     [Test]
-    public async Task GetAllAsync_Returns_NotFound()
+    public async Task GetAllAsync_ShouldBe_GetNoticias()
     {
         // Arrange
-        var requestUri = "/api/Noticias/GetAll";
+        var noticias = new List<NoticiaDto>
+            {
+                new NoticiaDto { Id = 1, Titulo = "Noticia 1" },
+                new NoticiaDto { Id = 2, Titulo = "Noticia 2" }
+            };
+
+        _repositoryMock.Setup(repo => repo.GetAllNoticias()).ReturnsAsync(noticias);
 
         // Act
-        var response = await _client.GetAsync(requestUri);
+        var result = await _controller.GetAllAsync();
 
         // Assert
-        Assert.AreEqual(HttpStatusCode.NotFound, response.StatusCode);
+        result.Should().NotBeNull();
+        result.Should().BeOfType<List<NoticiaDto>>();
+        result.Should().HaveCount(2);
     }
 }
---
+++ tests/Application.IntegrationTests/DatabaseSetup.cs
@@ -0,0 +1,35 @@
+﻿using Infrastructure.EntityFramework.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Application.IntegrationTests;
+public class DatabaseSetup : IDisposable
+{
+    private static readonly object _lock = new object();
+    private static bool _databaseInitialized;
+    private string dbName = "TestDatabase.db";
+
+    public static NoticiaDbContext CreateContext()
+    {
+        var config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .AddEnvironmentVariables()
+            .Build();
+
+        var databaseSettings = config.GetConnectionString("DefaultConnection");
+
+        var optionBuilder = new DbContextOptionsBuilder<NoticiaDbContext>();
+        optionBuilder.UseSqlServer(
+            databaseSettings,
+            optionsAction =>
+            {
+                optionsAction.EnableRetryOnFailure(10, TimeSpan.FromSeconds(10), null);
+            });
+
+        return new NoticiaDbContext(optionBuilder.Options);
+    }
+    public void Dispose()
+    {
+        throw new NotImplementedException();
+    }
+}
