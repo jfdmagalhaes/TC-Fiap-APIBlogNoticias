5b1df8359ca4dc50399ea25f1cb6f2247a88dfa9
Jessica <jessica.magalhaes@me.com.br>
2023-10-21 15:53:07 -0300

    adjust on integration repository tests

--- tests/Infrastructure.IntegrationTests/Repositories/NoticiaRepositoryTests.cs
+++ tests/Infrastructure.IntegrationTests/Repositories/NoticiaRepositoryTests.cs
@@ -5,6 +5,7 @@ using Domain.Repositories;
 using FluentAssertions;
 using Infrastructure.EntityFramework.Context;
 using Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
 using Xunit;
 
 namespace Infrastructure.IntegrationTests.Repositories;
@@ -13,7 +14,6 @@ public class NoticiaRepositoryTests : IClassFixture<DatabaseSetup>, IDisposable
 {
     private NoticiaDbContext _context;
     private INoticiaRepository _repository;
-    private IUnitOfWork _unitOfWork => _context;
 
     public NoticiaRepositoryTests(DatabaseSetup fixture)
     {
@@ -40,6 +40,53 @@ public class NoticiaRepositoryTests : IClassFixture<DatabaseSetup>, IDisposable
         //// Assert
         result.Should().NotBeNull();
         result.Should().BeOfType<List<NoticiaDto>>();
+    }
+
+    [Fact]
+    public async Task AddNoticia_ShouldBe_Successful()
+    {
+        //Arrange
+        var noticia = new NoticiaDto { Id = 1, Titulo = "Noticia 1" };
+
+        //Act
+        await _repository.AddNoticia(noticia);
+        await _context.SaveChangesAsync();
+
+
+        //Assert
+        var addedNoticia = await _context.Noticias.FirstOrDefaultAsync(n => n.Id == noticia.Id);
+        Assert.NotNull(addedNoticia);
+    }
+
+    [Fact]
+    public async Task DeleteNoticiaById_ShouldBe_Successful()
+    {
+        //Arrange
+        var noticia = new NoticiaDto { Id = 1, Titulo = "Noticia 1" };
+
+        //Act
+        await _repository.DeleteById(noticia.Id);
+        await _context.SaveChangesAsync();
+
+        //Assert
+        var getNoticia = await _context.Noticias.FirstOrDefaultAsync(n => n.Id == noticia.Id);
+        Assert.Null(getNoticia);
+    }
+
+    [Fact]
+    public async Task GetNoticiaById_ShouldBe_GetNoticia()
+    {
+        // Arrange
+        var noticia = new NoticiaDto { Id = 1, Titulo = "Noticia 1" };
+
+        _context.Add(noticia);
+        _context.SaveChanges();
+
+        var result = await _repository.GetNoticiaById(noticia.Id);
+
+        //// Assert
+        result.Should().NotBeNull();
+        result.Should().BeOfType<NoticiaDto>();
     }
 
     public void Dispose()
