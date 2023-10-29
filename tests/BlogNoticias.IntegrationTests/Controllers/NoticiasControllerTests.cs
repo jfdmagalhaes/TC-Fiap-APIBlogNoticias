92fb34352e8b8911b0545169b75abb5a717beca1
Jessica <jessica.magalhaes@me.com.br>
2023-10-29 12:13:36 -0300

    new tests on integrationTests, with database on container

--- tests/BlogNoticias.IntegrationTests/Controllers/NoticiasControllerTests.cs
+++ tests/BlogNoticias.IntegrationTests/Controllers/NoticiasControllerTests.cs
@@ -2,6 +2,11 @@
 using BlogNoticias.IntegrationTests.Helpers;
 using Domain.Entities;
 using Domain.Repositories;
+using Infrastructure.EntityFramework.Context;
+using Infrastructure.Repositories;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Moq;
 using Newtonsoft.Json;
 using System.Net.Http.Json;
@@ -9,65 +14,118 @@ using System.Text;
 using Xunit;
 
 namespace BlogNoticias.IntegrationTests.Controllers;
-public class NoticiasControllerTests : IClassFixture<ApiWebApplicationFactory> 
+public class NoticiasControllerTests : IClassFixture<DockerFixture> 
 {
     private readonly HttpClient _httpClient;
     private readonly Mock<INoticiaRepository> _repository = new();
+    private readonly DockerFixture _dockerFixture;
 
-    public NoticiasControllerTests(ApiWebApplicationFactory application)
+    //public NoticiasControllerTests(ApiWebApplicationFactory application)
+    //{
+    //    _httpClient = application.CreateClient();
+    //}
+
+    public NoticiasControllerTests(DockerFixture dockerFixture)
     {
-        _httpClient = application.CreateClient();
+        _dockerFixture = dockerFixture;
+
+        using (var connection = new SqlConnection(_dockerFixture.GetConnectionString()))
+        {
+
+
+            string createTableQuery = @"
+             CREATE TABLE Noticias (
+                Id INT PRIMARY KEY IDENTITY,
+                Titulo NVARCHAR(255) NOT NULL,
+                Conteudo NVARCHAR(MAX) NOT NULL,
+                DataPublicacao DATETIME NOT NULL,
+                Autor NVARCHAR(255) NOT NULL
+                );";
+
+            using (var command = new SqlCommand(createTableQuery, connection))
+            {
+                command.ExecuteNonQuery();
+            }
+        }
     }
+
 
     [Fact]
-    public async Task GetAll_ShouldExecute_Successfull()
+    public async void Should_Insert_Fi_With_Success()
     {
         // Arrange
-        var fixture = new Fixture();
-        var noticias = fixture.CreateMany<NoticiaDto>().ToList();
+        using (var dbContext = _dockerFixture.CreateDbContext())
+        {
+            var repository = new NoticiaRepository(dbContext);
 
-        _repository.Setup(repo => repo.GetAllNoticias()).ReturnsAsync(noticias);
+            var noticia = new NoticiaDto
+            {
+               Descricao = "Descricao"
+            };
 
-        // Act
-        var response = await _httpClient.GetAsync("/api/Noticias/GetAll");
+            // Act
+            await repository.AddNoticia(noticia);
+            await repository.UnitOfWork.CommitAsync();
 
-        // Assert
-        response.EnsureSuccessStatusCode();
+            // Assert
+            var insertedNoticia = await dbContext.Noticias.FindAsync(noticia.Id);
+            Assert.NotNull(insertedNoticia);
+            // Faça outras asserções de acordo com sua lógica de inserção e recuperação de dados
+        }
 
     }
 
-    [Fact]
-    public async Task GetById_ShouldExecute_Successfull()
-    {
-        // Arrange
-        var fixture = new Fixture();
-        var noticia = fixture.Create<NoticiaDto>();
 
-        _repository.Setup(repo => repo.GetNoticiaById(noticia.Id)).ReturnsAsync(noticia);
 
-        // Act
-        var response = await _httpClient.GetAsync($"/api/Noticias/{noticia.Id}");
+    //[Fact]
+    //public async Task GetAll_ShouldExecute_Successfull()
+    //{
+    //    // Arrange
+    //    var fixture = new Fixture();
+    //    var noticias = fixture.CreateMany<NoticiaDto>().ToList();
 
-        // Assert
-        response.EnsureSuccessStatusCode();
+    //    _repository.Setup(repo => repo.GetAllNoticias()).ReturnsAsync(noticias);
 
-    }
+    //    // Act
+    //    var response = await _httpClient.GetAsync("/api/Noticias/GetAll");
 
-    [Fact]
-    public async Task DeleteNoticia_ShouldExecute_Successfull()
-    {
-        // Arrange
-        var fixture = new Fixture();
-        var noticia = fixture.Create<NoticiaDto>();
+    //    // Assert
+    //    response.EnsureSuccessStatusCode();
 
-        _repository.Setup(repo => repo.DeleteById(noticia.Id));
+    //}
 
-        // Act
-        var content = new StringContent(JsonConvert.SerializeObject(noticia), Encoding.UTF8, "application/json");
-        var response = await _httpClient.PutAsync($"/api/Noticias/{noticia.Id}", content);
+    //[Fact]
+    //public async Task GetById_ShouldExecute_Successfull()
+    //{
+    //    // Arrange
+    //    var fixture = new Fixture();
+    //    var noticia = fixture.Create<NoticiaDto>();
 
-        // Assert
-        response.EnsureSuccessStatusCode();
-    }
+    //    _repository.Setup(repo => repo.GetNoticiaById(noticia.Id)).ReturnsAsync(noticia);
+
+    //    // Act
+    //    var response = await _httpClient.GetAsync($"/api/Noticias/{noticia.Id}");
+
+    //    // Assert
+    //    response.EnsureSuccessStatusCode();
+
+    //}
+
+    //[Fact]
+    //public async Task DeleteNoticia_ShouldExecute_Successfull()
+    //{
+    //    // Arrange
+    //    var fixture = new Fixture();
+    //    var noticia = fixture.Create<NoticiaDto>();
+
+    //    _repository.Setup(repo => repo.DeleteById(noticia.Id));
+
+    //    // Act
+    //    var content = new StringContent(JsonConvert.SerializeObject(noticia), Encoding.UTF8, "application/json");
+    //    var response = await _httpClient.PutAsync($"/api/Noticias/{noticia.Id}", content);
+
+    //    // Assert
+    //    response.EnsureSuccessStatusCode();
+    //}
 
 }
---
+++ tests/BlogNoticias.IntegrationTests/Helpers/DockerFixture.cs
@@ -0,0 +1,56 @@
+﻿using Docker.DotNet;
+using Docker.DotNet.Models;
+using Infrastructure.EntityFramework.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogNoticias.IntegrationTests.Helpers;
+public class DockerFixture : IDisposable
+{
+    private DockerClient _dockerClient;
+    private string _containerId;
+
+    public DockerFixture()
+    {
+        _dockerClient = new DockerClientConfiguration().CreateClient();
+
+        var createContainerResponse = _dockerClient.Containers.CreateContainerAsync(new CreateContainerParameters
+        {
+            Image = "mcr.microsoft.com/mssql/server:2019-latest",
+            Env = new List<string> { "SA_PASSWORD=Pass@word" },
+            HostConfig = new HostConfig
+            {
+                PortBindings = new Dictionary<string, IList<PortBinding>>()
+                {
+                    { "1433/tcp", new List<PortBinding> { new PortBinding { HostPort = "1433" } } }
+                },
+                PublishAllPorts = true // Optional: Set this to true if you want to publish all exposed ports
+            }
+        }).GetAwaiter().GetResult();
+
+
+        _containerId = createContainerResponse.ID;
+        _dockerClient.Containers.StartContainerAsync(_containerId, new ContainerStartParameters()).GetAwaiter().GetResult();
+    }
+
+    public string GetConnectionString()
+    {
+        var _connectionString = $"Server=localhost,1433;Database=db_noticiastest;User=sa;Password=Pass@word;";
+        return _connectionString;
+    }
+
+    public NoticiaDbContext CreateDbContext()
+    {
+        var options = new DbContextOptionsBuilder<NoticiaDbContext>()
+            .UseSqlServer(GetConnectionString())
+            .Options;
+
+        return new NoticiaDbContext(options);
+    }
+
+    public void Dispose()
+    {
+        _dockerClient.Containers.StopContainerAsync(_containerId, new ContainerStopParameters()).GetAwaiter().GetResult();
+        _dockerClient.Containers.RemoveContainerAsync(_containerId, new ContainerRemoveParameters()).GetAwaiter().GetResult();
+        _dockerClient.Dispose();
+    }
+}
