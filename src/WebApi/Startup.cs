da36172fc09acbe95d74f90d8974d65fdc1dfa34
Jessica <jessica.magalhaes@me.com.br>
2023-11-02 16:53:31 -0300

    adjust startup

--- src/WebApi/Startup.cs
+++ src/WebApi/Startup.cs
@@ -51,13 +51,14 @@ public class Startup
             endpoints.MapRazorPages();
         });
 
-        //using (var scope = app.ApplicationServices.CreateScope())
-        //{
-        //    var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>(); // Substitua YourDbContext pelo nome do seu DbContext.
+        using (var scope = app.ApplicationServices.CreateScope())
+        {
+            var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>(); // Substitua YourDbContext pelo nome do seu DbContext.
 
-        //    if (dbContext.Database.IsRelational())         
-            
-        //}
+            if (dbContext.Database.IsRelational())
                 ServiceExtension.MigrationInitialization(app);
+
+
+        }
     }
 }
